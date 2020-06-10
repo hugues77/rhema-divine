d8c75bb871e87825e6125a4b7a185be912dfd44a
Hugues eyong <hugues777@outlook.com>
2020-06-10 18:36:35 +0200

    test push

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+    session_start();
+    include 'functions/main-functions.php';
+    
+    $pages = scandir('pages/');
+    if(isset($_GET['page']) && !empty($_GET['page'])){
+      if(in_array($_GET['page']. '.php',$pages)){
+        $page = $_GET['page'];
+      }else{
+        $page = "error";
+      }
+    }else{
+      $page = "index";
+    }
+
+
+    $pages_functions = scandir('functions/');
+    if(in_array($page.'.func.php',$pages_functions)){
+      include 'functions/'.$page.'.func.php';
+    }
+?>
+
+<?php require_once 'body/topbar.php'; ?><br>
+
+<!-- entete image -->
+  <section class="mt-5 phone_cache">
+    <img class="card-img-top mt-4" src="images/rhemaEntete.png" alt="rhema divine" width="1350px" height="450px" >
+  </section>
+<!-- fin entete image --> 
+
+<?php include 'pages/'.$page.'.php'; 
+require_once 'pages/creation_site.php';
+require_once 'footer.php';
+?>
