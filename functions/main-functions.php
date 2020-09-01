6bea79a37171b07d14bd751b4ef078286b7bc648
Hugues eyong <hugues777@outlook.com>
2020-09-02 01:08:16 +0200

    correction de la base de données

--- functions/main-functions.php
+++ functions/main-functions.php
@@ -3,7 +3,7 @@
     //connexion de la base de données en local
     //------------------------------------------
 
- /*    $dbhost = 'localhost';
+    $dbhost = 'localhost';
     $dbname = 'rhema';
     $dbuser = 'root';
     $dbpswd = '';
@@ -12,13 +12,13 @@
         $connexion = new PDO('mysql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpswd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING));
     }catch (PDOException $e){
             die("Erreur de connexion à la base de données, Merci");
-    } */
+    } 
 
 
     //--------------------------------------------
     //connexion de la base de données en ligne
     //------------------------------------------
-    $dbhost = '185.98.131.128';
+/*     $dbhost = '185.98.131.128';
     $dbname = 'rhema1369589';
     $dbuser = 'rhema1369589';
     $dbpswd = 'pkol3hkr00';
@@ -26,6 +26,7 @@
    try{
        $connexion = new PDO('mysql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpswd,array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8', PDO::ATTR_ERRMODE =>PDO::ERRMODE_WARNING));
    }catch (PDOException $e){
-           die("Erreur de connexion à la base de données, Merci");
-   }
+           die("Erreur de connexion à la base de données, Merci de reessayer");
+   }  
+ */
 ?>
